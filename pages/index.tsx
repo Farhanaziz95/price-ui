46e9a5a79d7ab199f2d36c578820aef8a07e4763
Farhan Aziz <farhanazizumrani95@gmail.com>
2023-03-20 05:31:37 +0500

    made responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,12 +18,12 @@ export default function Home() {
       </Head>
 
       <Box>
-        <Flex direction="column">
+        <Flex direction={{ base: 'column', lg: 'column' }}>
           <Box height="50vh" w="100%" bg="#6B46C1"></Box>
           <Box height="50vh" w="100%" bg="#fff"></Box>
         </Flex>
         <Container>
-          <Box position="fixed" zIndex="2" left="72" top="24">
+          <Box position={{ base: 'absolute', lg: 'fixed' }} zIndex="2" left={{base:"3",lg:"72"}} top={[14,0,24]}>
             <Center>
               <Heading color="white" pb="4">
                 Simple pricing for your business
@@ -33,13 +33,14 @@ export default function Home() {
               <Text color="white" pb="8">Plans that are carefully crafted to suit your business.</Text>
             </Center>
             <Card
-              direction={{ base: 'column', sm: 'row' }}
+              direction={{ base: 'column', lg: 'row' }}
               overflow='hidden'
               variant='outline'
+              w={['96%','100%','100%']}
 
 
             >
-              <Stack p="2" pb="4" bg="#F0EAFB" w="40%">
+              <Stack p="2" pb="4" bg="#F0EAFB" w={['100%','40%','40%']}>
                 <CardBody>
                   <Center>
                     <Text
@@ -97,24 +98,24 @@ export default function Home() {
             </Card>
           </Box>
 
-          <Flex position="fixed" w="80%" bottom="20" left="36" justifyContent="space-around">
-            <Flex>
+          <Flex position={{base:"absoulte",lg:"fixed"}} direction={{ base: 'column', lg: 'row' }} w={['100%',"80%"]} bottom={["0","20"]} mt={{base:"6",lg:"1"}} left="36" justifyContent="space-around">
+            <Flex pb={{base:'6'}}>
               <Image
                 src='/guardstar.png'
                 alt='guard star icon'
               />
-              <Text w="-webkit-fill-available" pl="3" pt="0.5" fontWeight="bold" fontSize="small">
-                30 days money back <br /> Guarantee
+              <Text w={['-moz-fit-content',"-webkit-fill-available"]} pl="3" pt="0.5" fontWeight="bold" fontSize="small">
+                30 days money back  Guarantee
               </Text>
             </Flex>
 
-            <Flex>
+            <Flex pb={{base:'6'}}>
               <Image
                 src='/pricetagstar.png'
                 alt='guard star icon'
               />
               <Text w="-webkit-fill-available" pl="3" pt="0.5" fontWeight="bold" fontSize="small">
-                No setup fees <br />
+                No setup fees 
                 100% hassle-free
               </Text>
             </Flex>
@@ -125,7 +126,7 @@ export default function Home() {
                 alt='guard star icon'
               />
               <Text w="-webkit-fill-available" pl="3" pt="0.5" fontWeight="bold" fontSize="small">
-                No monthly subscription <br />
+                No monthly subscription
                 Pay once and for all
               </Text>
             </Flex>
